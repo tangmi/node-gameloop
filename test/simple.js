f81c7b0172ee481386993bd874d4100a143356ff
norlin <alexey@norlin.guru>
2017-02-20 11:41:58 +0300

    Convert loop from Date.now() to process.hrtime
    increase precision
    0.344 -> 0.334 (for 30fps)
    0.1823 -> 0.1685 (for 60fps)
    
    decrease cpu usage
    ~28% -> ~10% (30fps)

--- test/simple.js
+++ test/simple.js
@@ -1,20 +1,35 @@
-var usage = require('usage');
+const usage = require('usage');
+const gameloop = require('..');
 
-var gameloop = require('..');
+let totalDelta = 0;
+let testRuns = 0;
+
+let fps = 60;
+let intervalMs = 1000 / fps;
 
 gameloop.setGameLoop(function(delta) {
-	console.log('delta=%s', delta)
-}, 1000 / 30);
+	console.log(`delta=${delta}`);
 
-var testCount = 0,
-	totalCpu = 0;;
+	totalDelta += delta;
+	testRuns += 1;
+}, intervalMs);
+
+let testCount = 0;
+let totalCpu = 0;
+
 setInterval(function() {
-	testCount++;
+	testCount += 1;
+
 	usage.lookup(process.pid, function(err, result) {
-		console.log('sample %s cpu: %s%%', testCount, result.cpu);
 		totalCpu += result.cpu;
-		if (testCount == 3) {
-			console.log('average cpu over %s seconds: %s%%', testCount, totalCpu / testCount);
+
+		if (testCount == 10) {
+			let avgDelta = totalDelta / testRuns;
+			let targetDelta =
+			console.log(`Test runs: ${testRuns}`);
+			console.log(`target delta : ${intervalMs/1000}s`);
+			console.log(`average delta: ${avgDelta}s`);
+			console.log(`average cpu over ${testCount} seconds: ${totalCpu / testCount}%`);
 			process.exit(0);
 		}
 	});
