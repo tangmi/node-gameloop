91fd5038b8e812c563aa6399d6ffebec6d55dc2d
Michael Tang <tangmi@uw.edu>
2017-03-05 13:18:38 -0800

    Attempt to fix memory leak

---
+++ test/memory.js
@@ -0,0 +1,45 @@
+const pidusage = require('pidusage');
+const gameloop = require('..');
+
+let fps = 60;
+let intervalMs = 1000 / fps;
+
+gameloop.setGameLoop(function(delta) {}, intervalMs);
+
+let testCount = 0;
+
+let memorySamples = [];
+
+const count = 25;
+console.log(`collecting samples for ${count} seconds`)
+setInterval(function() {
+	testCount += 1;
+
+	pidusage.stat(process.pid, function(err, result) {
+		memorySamples.push(result.memory);
+
+		if (testCount == count) {
+			let hasReduction = false
+
+			console.log('memory usage:');
+			console.log(memorySamples[0]);
+			for (var i = 1; i < memorySamples.length; i++) {
+				var memorySample = memorySamples[i];
+				var delta = memorySample - memorySamples[i - 1];
+				console.log(`${memorySample} (delta: ${delta})`);
+
+				if (delta < 0) {
+					hasReduction = true;
+				}
+			}
+
+			if (hasReduction) {
+				console.log('memory likely not leaking! :)');
+			} else {
+				console.log('memory may be leaking!');
+			}
+
+			process.exit(0);
+		}
+	});
+}, 1000);
